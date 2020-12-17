2eba9f2bdc1c037525f0161e26cf4ebcc8ba1bd5
shreyasood <shreya.sood123@gmail.com>
2020-12-17 22:34:13 +0530

    13 done

--- donate.php
+++ donate.php
@@ -225,6 +225,9 @@ if(isset($_POST['submit']))
 	  		</div>';
 		}
 		$DonorDOB = $year."-".$month."-".$day;
+
+		$password = md5($password);
+
 		if (isset($name) && isset($blood_group) && isset($gender) && isset($day) && isset($month) && isset($year) && isset($email) && isset($contact) && isset($city) && isset($password)) 
 		{
 			$sql="INSERT INTO blooddonation VALUES ('$name','$blood_group','$gender','$DonorDOB','$email','$contact','$city','$password','0')";
@@ -312,13 +315,15 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
 				<form class="form-group" action="" method="post" novalidate="">
 					<div class="form-group">
 						<label for="fullname">Full Name</label>
-						<input type="text" name="name" id="fullname" placeholder="Full Name" required pattern="[A-Za-z/\s]+" title="Only lower and upper case and space" class="form-control">
+						<input type="text" name="name" id="fullname" placeholder="Full Name" required pattern="[A-Za-z/\s]+" title="Only lower and upper case and space" class="form-control" value="<?php if(isset($name)) echo $name; ?>">
+						
 						<?php if(isset($nameError)) echo $nameError; ?>
 					</div><!--full name-->
 					<div class="form-group">
               <label for="name">Blood Group</label><br>
               <select class="form-control demo-default" id="blood_group" name="blood_group" required>
                 <option value="">---Select Your Blood Group---</option>
+				<?php if(isset($blood_group)) echo '<option selected="" value="'.$blood_group.'">'.$blood_group.'</option>';  ?>
                 <option value="A+">A+</option>
                 <option value="A-">A-</option>
                 <option value="B+">B+</option>
@@ -333,21 +338,27 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
 					<div class="form-group">
 				              <label for="gender">Gender</label><br>
 				              		Male<input type="radio" name="gender" id="gender" value="Male" style="margin-left:10px; margin-right:10px;" checked>
-				              		Fe-male<input type="radio" name="gender" id="gender" value="Fe-male" style="margin-left:10px;">
+				              		Female<input type="radio" name="gender" id="gender" value="Female" style="margin-left:10px;" <?php if(isset($gender)){if($gender=="Female") echo "checked";}   ?> >
+									  
 				              		<?php if(isset($genderError)) echo $genderError; ?>
 				    </div><!--gender-->
 				    <div class="form-inline">
               <label for="name">Date of Birth</label><br>
               <select class="form-control demo-default" id="date" name="day" style="margin-bottom:10px;" required>
+
                 <option value="">---Date---</option>
+				<?php if(isset($day)) echo '<option selected="" value="'.$day.'">'.$day.'</option>';  ?>
+
                 <option value="01" >01</option><option value="02" >02</option><option value="03" >03</option><option value="04" >04</option><option value="05" >05</option><option value="06" >06</option><option value="07" >07</option> <option value="08" >08</option><option value="09" >09</option><option value="10" >10</option><option value="11" >11</option><option value="12" >12</option><option value="13" >13</option><option value="14" >14</option><option value="15" >15</option><option value="16" >16</option><option value="17" >17</option><option value="18" >18</option><option value="19" >19</option><option value="20" >20</option><option value="21" >21</option><option value="22" >22</option><option value="23" >23</option><option value="24" >24</option><option value="25" >25</option><option value="26" >26</option><option value="27" >27</option><option value="28" >28</option><option value="29" >29</option><option value="30" >30</option><option value="31" >31</option>
               </select>
               <select class="form-control demo-default" name="month" id="month" style="margin-bottom:10px;" required>
                 <option value="">---Month---</option>
+				<?php if(isset($month)) echo '<option selected="" value="'.$month.'">'.$month.'</option>';  ?>
                 <option value="01" >January</option><option value="02" >February</option><option value="03" >March</option><option value="04" >April</option><option value="05" >May</option><option value="06" >June</option><option value="07" >July</option><option value="08" >August</option><option value="09" >September</option><option value="10" >October</option><option value="11" >November</option><option value="12" >December</option>
               </select>
               <select class="form-control demo-default" id="year" name="year" style="margin-bottom:10px;" required>
                 <option value="">---Year---</option>
+				<?php if(isset($year)) echo '<option selected="" value="'.$year.'">'.$year.'</option>';  ?>
                 <option value="1957" >1957</option><option value="1958" >1958</option><option value="1959" >1959</option><option value="1960" >1960</option><option value="1961" >1961</option><option value="1962" >1962</option><option value="1963" >1963</option><option value="1964" >1964</option><option value="1965" >1965</option><option value="1966" >1966</option><option value="1967" >1967</option><option value="1968" >1968</option><option value="1969" >1969</option><option value="1970" >1970</option><option value="1971" >1971</option><option value="1972" >1972</option><option value="1973" >1973</option><option value="1974" >1974</option><option value="1975" >1975</option><option value="1976" >1976</option><option value="1977" >1977</option><option value="1978" >1978</option><option value="1979" >1979</option><option value="1980" >1980</option><option value="1981" >1981</option><option value="1982" >1982</option><option value="1983" >1983</option><option value="1984" >1984</option><option value="1985" >1985</option><option value="1986" >1986</option><option value="1987" >1987</option><option value="1988" >1988</option><option value="1989" >1989</option><option value="1990" >1990</option><option value="1991" >1991</option><option value="1992" >1992</option><option value="1993" >1993</option><option value="1994" >1994</option><option value="1995" >1995</option><option value="1996" >1996</option><option value="1997" >1997</option><option value="1998" >1998</option><option value="1999" >1999</option><option value="2000" >2000</option><option value="2001" >2001</option><option value="2002" >2002</option><option value="2003" >2003</option>
               </select>
               <?php if(isset($dayError)) echo $dayError; ?>
@@ -356,18 +367,18 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
             </div><!--End form-group-->
 				    <div class="form-group">
 						<label for="fullname">Email</label>
-						<input type="text" name="email" id="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Please write correct email" class="form-control">
+						<input type="text" name="email" id="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Please write correct email" class="form-control" value="<?php if(isset($email)) echo $email; ?>">
 						<?php if(isset($emailError)) echo $emailError; ?>
 					</div>
 					<div class="form-group">
               <label for="contact_no">Contact No</label>
-              <input type="text" name="contact_no" value="" placeholder="03********" class="form-control" required pattern="^\d{10}$" title="10 numeric characters only" maxlength="10">
+              <input type="text" name="contact_no" placeholder="03********" class="form-control" required pattern="^\d{10}$" title="10 numeric characters only" maxlength="10" value="<?php if(isset($contact)) echo $contact; ?>">
               <?php if(isset($contactError)) echo $contactError; ?>
             </div><!--End form-group-->
 					<div class="form-group">
               <label for="city">City</label>
               <select name="city" id="city" class="form-control demo-default" required>
-				<option value="">-- Select --</option><option value="AndhraPradesh" >Andhra Pradesh</option><option value="ArunachalPradesh" >Arunachal Pradesh</option><option value="Assam" >Assam</option><option value="Bihar" >Bihar</option><option value="Chhattisgarh" >Chhattisgarh</option><option value="Goa" >Goa</option><option value="Gujarat" >Gujarat</option><option value="Haryana" >Haryana</option><option value="HimachalPradesh" >Himachal Pradesh</option><option value="Jharkhand" >Jharkhand</option><option value="Karnataka" >Karnataka</option><option value="Kerala" >Kerala</option><option value="MadhyaPradesh" >Madhya Pradesh</option><option value="Maharashtra" >Maharashtra</option><option value="Manipur" >Manipur</option><option value="Meghalaya" >Meghalaya</option><option value="Mizoram" >Mizoram</option><option value="Nagaland" >Nagaland</option><option value="Orissa" >Orissa</option><option value="Punjab" >Punjab</option><option value="Rajasthan" >Rajasthan</option><option value="Sikkim" >Sikkim</option><option value="TamilNadu" >Tamil Nadu</option><option value="Telangana" >Telangana</option><option value="Tripura" >Tripura</option><option value="UttarPradesh" >Uttar Pradesh</option><option value="Uttarakhand" >Uttarakhand</option><option value="WestBengal" >West Bengal</option><option value="Andaman&Nicobar" >Andaman and Nicobar Islands</option><option value="Chandigarh" >Chandigarh</option><option value="Dadra" >Dadra and Nagar Haveli</option><option value="Daman" >Daman and Diu</option><option value="Delhi" >Delhi</option><option value="Jammu" >Jammu and Kashmir</option><option value="Ladakh" >Ladakh</option><option value="Lakshwadeep" >Lakshwadeep</option><option value="Puducherry" >Puducherry</option>
+				<option value="">-- Select --</option><?php if(isset($city)) echo '<option selected="" value="'.$city.'">'.$city.'</option>';  ?><option value="AndhraPradesh" >Andhra Pradesh</option><option value="ArunachalPradesh" >Arunachal Pradesh</option><option value="Assam" >Assam</option><option value="Bihar" >Bihar</option><option value="Chhattisgarh" >Chhattisgarh</option><option value="Goa" >Goa</option><option value="Gujarat" >Gujarat</option><option value="Haryana" >Haryana</option><option value="HimachalPradesh" >Himachal Pradesh</option><option value="Jharkhand" >Jharkhand</option><option value="Karnataka" >Karnataka</option><option value="Kerala" >Kerala</option><option value="MadhyaPradesh" >Madhya Pradesh</option><option value="Maharashtra" >Maharashtra</option><option value="Manipur" >Manipur</option><option value="Meghalaya" >Meghalaya</option><option value="Mizoram" >Mizoram</option><option value="Nagaland" >Nagaland</option><option value="Orissa" >Orissa</option><option value="Punjab" >Punjab</option><option value="Rajasthan" >Rajasthan</option><option value="Sikkim" >Sikkim</option><option value="TamilNadu" >Tamil Nadu</option><option value="Telangana" >Telangana</option><option value="Tripura" >Tripura</option><option value="UttarPradesh" >Uttar Pradesh</option><option value="Uttarakhand" >Uttarakhand</option><option value="WestBengal" >West Bengal</option><option value="Andaman&Nicobar" >Andaman and Nicobar Islands</option><option value="Chandigarh" >Chandigarh</option><option value="Dadra" >Dadra and Nagar Haveli</option><option value="Daman" >Daman and Diu</option><option value="Delhi" >Delhi</option><option value="Jammu" >Jammu and Kashmir</option><option value="Ladakh" >Ladakh</option><option value="Lakshwadeep" >Lakshwadeep</option><option value="Puducherry" >Puducherry</option>
 				<?php if(isset($cityError)) echo $cityError; ?>
             </div><!--city end-->
 
@@ -381,7 +392,7 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
               <input type="password" name="c_password" value="" placeholder="Confirm Password" class="form-control" required pattern=".{6,10}">
             </div><!--End form-group-->
             <div class="form-inline">
-              <input type="checkbox" name="term" value="true" required style="margin-left:10px;">
+              <input type="checkbox" checked="" name="term" value="true" required style="margin-left:10px;">
               <span style="margin-left:10px;"><b>I agree to donate my blood and show my Name, Contact Nos. and E-Mail in Blood donors List</b></span>
             </div><!--End form-group-->
 			
