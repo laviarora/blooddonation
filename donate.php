30e1ea9f8429d72bf3d261eeecb8e857d0f48c09
Ishita Gupta <53005952+IshitaGupta27@users.noreply.github.com>
2020-12-17 20:05:40 +0530

    Update donate.php

--- donate.php
+++ donate.php
@@ -239,13 +239,12 @@ if(isset($_POST['submit']))
 			}
 			else
 			{
-				echo mysqli_error($connection);
-				//$submitError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-				//<strong>Data not inserted. Try again.</strong>
-				//<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		  		//<span aria-hidden="true">&times;</span>
-				//</button>
-	  			//</div>';
+				$submitError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+				<strong>Data not inserted. Try again.</strong>
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		  		<span aria-hidden="true">&times;</span>
+				</button>
+	  			</div>';
 			}
 		}
 	}
