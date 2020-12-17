1b33fe3ef94ce5a8bf0bfb6c59d89f21ca8e8ed4
shambhavisriv <shambhavisrivastava010@gmail.com>
2020-12-18 02:46:04 +0530

    16

--- include/header.php
+++ include/header.php
@@ -35,5 +35,14 @@
 
 <?php 
             include 'config.php';
-            include 'navigation.php';
+
+            session_start();
+            if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){
+                include 'usernav.php';
+            }
+            else{
+                include 'navigation.php';
+            }
+
+            
 ?>
--- signin.php
+++ signin.php
@@ -2,6 +2,62 @@
 
 	//include header file
 	include ('include/header.php');
+	if(isset($_POST['SignIn'])){
+
+		if(isset($_POST['email']) && !empty($_POST['email']))
+		{
+			$email=$_POST['email'];
+		}
+		else
+		{
+			$passwordError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			<strong>Fill the email...</strong>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+	  		</div>';
+		}
+		if(isset($_POST['password']) && !empty($_POST['password']))
+		{
+			$password=$_POST['password'];
+			$password = md5($password);
+		}
+		else
+		{
+			$passwordError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			<strong>Fill the Password</strong>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+	  		</div>';
+		}
+
+		if(isset($email) && isset($password)){
+			$sql = "SELECT * FROM donor WHERE password = '$password' and email = '$email'";
+			$result = mysqli_query($connection,$sql);
+			if(mysqli_num_rows($result) >0){
+				while($row = mysqli_fetch_assoc($result)){
+					$_SESSION['user_id'] = $row['id'];
+					$_SESSION['name'] = $row['name'];
+					
+
+					header('Location: user/index.php');
+				}
+			
+			}else{
+				$submitError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			<strong>No Record Found</strong>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+	  		</div>';
+			}
+		}
+
+	
+
+	}
+
 
 ?>
 
@@ -47,17 +103,21 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
 		<div class="col-md-6 offset-md-3 form-container">
 		<h3>SignIn</h3>
 		<hr class="red-bar">
+		<?php if(isset($submitError)) echo $submitError; ?>
+		
 		
 		<!-- Erorr Messages -->
 
 			<form action="" method="post" >
 				<div class="form-group">
 					<label for="email">Email/Phone no.</label>
-					<input type="text" name="email_phone" class="form-control" placeholder="Email Or Phone" required>
+					<input type="text" name="email" class="form-control" placeholder="Email" required>
+					<?php if(isset($emailError)) echo $emailError; ?>
 				</div>
 				<div class="form-group">
 					<label for="password">Password</label>
 					<input type="password" name="password" placeholder="Password" required class="form-control">
+					<?php if(isset($passwordError)) echo $passwordError; ?>
 				</div>
 				<div class="form-group">
 					<button class="btn btn-danger btn-lg center-aligned" type="submit" name="SignIn">SignIn</button>
