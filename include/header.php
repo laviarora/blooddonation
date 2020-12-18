58885206f27237235b671a9877fdd5a30d5211b4
Ishita Gupta <53005952+IshitaGupta27@users.noreply.github.com>
2020-12-18 17:02:11 +0530

    Revert "21 done"
    This reverts commit 0c83a6c1b0b09ff6e63cc1e264661d4aab9bf940.

--- include/header.php
+++ include/header.php
@@ -37,7 +37,6 @@
             include 'config.php';
 
             session_start();
-            include('../include/config.php');
             if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){
                 include 'usernav.php';
             }
